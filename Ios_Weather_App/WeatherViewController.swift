881fa95d864e2238d34b3ae0dca323b0cfac3ca0
Kaan Arslan <kaan.arslan@ug.bilkent.edu.tr>
2024-06-03 22:16:05 +0300

    UI added.

---
+++ Ios_Weather_App/WeatherViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Ios_Weather_App
+//
+//  Created by kaan2 on 3.06.2024.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    
+    @IBOutlet weak var tempatureLabel: UILabel!
+    
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    @IBAction func addLocationButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    @IBAction func locationButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
